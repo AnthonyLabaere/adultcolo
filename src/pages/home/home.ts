0e822e53c800c852d886d090d745d8f3e759a359
Anthony Labaere <anthony.labaere@gmail.com>
2018-09-13 19:39:35 +0200

    Ajout de la possibilité de renseigner 3 joueurs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GamePage } from '../game/game';
 
@@ -6,10 +6,19 @@ import { GamePage } from '../game/game';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  public players: string[];
 
   constructor(public navCtrl: NavController) {
 
+  }
+  
+  ngOnInit() {
+    this.players = [];
+    for (let i = 0; i < 3; i++) {
+      this.players.push('');
+    }
   }
 
   public onPlayClick() {
