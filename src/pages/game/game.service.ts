caf8119fbeba3170b24da26c959a34bacb29cdb8
Anthony Labaere <anthony.labaere@gmail.com>
2018-09-14 18:49:38 +0200

    Ajout d'un premier tri sur les conditions à mettre en jeu

--- src/pages/game/game.service.ts
+++ src/pages/game/game.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from "@angular/core";
+import * as _ from 'lodash';
 import { Condition, Turn, TurnType } from "../../app/entities";
 import { ConditionService } from "../../app/_services/condition.service";
 
 
 @Injectable()
 export class GameService {
+
+    private static CONDITIONS_BY_GAME:number = 5;
 
     constructor(private conditionService: ConditionService) {
 
@@ -15,7 +18,7 @@ export class GameService {
 
         return this.conditionService.getConditions()
             .then((conditions: Condition[]) => {
-                conditions.forEach((condition: Condition) => {
+                _.shuffle(conditions).slice(0, GameService.CONDITIONS_BY_GAME - 1).forEach((condition: Condition) => {
                     turns.push(new Turn(TurnType.Condition, condition.theme, condition.labels.general));
                 });
                 
