06b0eea72767a86a998778d88553fb73c6edbc4d
Anthony Labaere <anthony.labaere@gmail.com>
2018-09-30 15:05:38 +0200

    Ajout de paramétrage selon les environnements

--- src/app/entities.ts
+++ src/app/entities.ts
@@ -1,4 +1,5 @@
 import { CommonService } from "./_services/common.service";
+import { environment as ENV } from '../environments/environment';
 
 //region "DTO"
 
@@ -130,7 +131,6 @@ export class Player {
 //region data
 
 export abstract class TurnEntryData {
-    // TODO : retirer les thèmes, ça sera probablement toujours inutile 
     theme: number;
     labels: string[];
 }
@@ -169,23 +169,17 @@ export class LocalizedString {
 }
 
 export class Timer {
-    // TODO : à mettre ailleurs ?
-    private static TIMER_TIME_SECONDS_WITH_PLAYERS = 10; 
-    private static TIMER_TIME_SECONDS_WITHOUT_PLAYERS = 20; 
-
-    private static INTERVAL_IN_MILLI_SECONDS = 10;
-
     totalTimeInMilliSeconds: number;
     timeInMilliSecondsLeft: number;
     interval;
 
     constructor(withPlayers: boolean) {
         if (withPlayers) {
-            this.totalTimeInMilliSeconds = 1000 * Timer.TIMER_TIME_SECONDS_WITH_PLAYERS;
-            this.timeInMilliSecondsLeft = 1000 * Timer.TIMER_TIME_SECONDS_WITH_PLAYERS;
+            this.totalTimeInMilliSeconds = 1000 * ENV.TIMER_TIME_SECONDS_WITH_PLAYERS;
+            this.timeInMilliSecondsLeft = 1000 * ENV.TIMER_TIME_SECONDS_WITH_PLAYERS;
         } else {
-            this.totalTimeInMilliSeconds = 1000 * Timer.TIMER_TIME_SECONDS_WITHOUT_PLAYERS;
-            this.timeInMilliSecondsLeft = 1000 * Timer.TIMER_TIME_SECONDS_WITHOUT_PLAYERS;
+            this.totalTimeInMilliSeconds = 1000 * ENV.TIMER_TIME_SECONDS_WITHOUT_PLAYERS;
+            this.timeInMilliSecondsLeft = 1000 * ENV.TIMER_TIME_SECONDS_WITHOUT_PLAYERS;
         }
 
     }
@@ -194,12 +188,12 @@ export class Timer {
     start(callback: Function) {
         this.interval = setInterval(() => {
             if (this.timeInMilliSecondsLeft > 0) {
-                this.timeInMilliSecondsLeft = this.timeInMilliSecondsLeft - Timer.INTERVAL_IN_MILLI_SECONDS;
+                this.timeInMilliSecondsLeft = this.timeInMilliSecondsLeft - ENV.TIMER_INTERVAL_IN_MILLI_SECONDS;
             } else {
                 this.stop();
                 callback();
             }
-        }, Timer.INTERVAL_IN_MILLI_SECONDS);
+        }, ENV.TIMER_INTERVAL_IN_MILLI_SECONDS);
     }
 
     stop() {
--- src/pages/play/play.service.ts
+++ src/pages/play/play.service.ts
@@ -3,25 +3,12 @@ import * as _ from 'lodash';
 import { Condition, ForOrAgainst, Game, Instead, List, LongWinded, Song, Turn, TurnEntry, TurnType } from "../../app/entities";
 import { PlayerService } from "../../app/_services/player.service";
 import { TurnEntryService } from "../../app/_services/turnEntry.service";
+import { environment as ENV } from '../../environments/environment';
 
 @Injectable()
 export class PlayService {
 
-    // TODO : à mettre dans des paramètres
-    private static TURN_NUMBER_TOTAL = 40;
-
-    private static FOR_OR_AGAINSTS_BY_PLAY:number[] = [1, 3]; // Entre 1 et 3
-    private static GAMES_BY_PLAY:number[] = [2, 4]; // Entre 2 et 4
-    private static INSTEADS_BY_PLAY:number[] = [1, 3]; // Entre 1 et 3
-    private static LISTS_BY_PLAY:number[] = [1, 3]; // Entre 1 et 3
-    private static LONG_WINDEDS_BY_PLAY:number[] = [2, 4]; // Entre 2 et 4
-    private static SONGS_BY_PLAY:number[] = [2, 3]; // Entre 2 et 3
-
-    private static MIN_TURNS_AFTER_LONG_WINDED_END = 5;
-    private static MAX_PERCENT_TURNS_BEFORE_ANY_LONG_WINDED_TURN = 0.75;
-
     constructor(private playerService: PlayerService, private turnEntryService: TurnEntryService) {
-        
     }
 
     public getTurns(): Promise<Turn[]> {
@@ -31,44 +18,44 @@ export class PlayService {
         return this.turnEntryService.getTurnEntries(TurnType.FOR_OR_AGAINST)
             .then((forOrAgainsts: ForOrAgainst[]) => {
                 turns = turns.concat(this.getTurnFormTurnEntries(TurnType.FOR_OR_AGAINST, forOrAgainsts, 
-                    _.random(PlayService.FOR_OR_AGAINSTS_BY_PLAY[0], PlayService.FOR_OR_AGAINSTS_BY_PLAY[1])));
+                    _.random(ENV.FOR_OR_AGAINSTS_BY_PLAY[0], ENV.FOR_OR_AGAINSTS_BY_PLAY[1])));
 
                 return this.turnEntryService.getTurnEntries(TurnType.GAME);
             })
             .then((games: Game[]) => {
                 turns = turns.concat(this.getTurnFormTurnEntries(TurnType.GAME, games,
-                    _.random(PlayService.GAMES_BY_PLAY[0], PlayService.GAMES_BY_PLAY[1])));
+                    _.random(ENV.GAMES_BY_PLAY[0], ENV.GAMES_BY_PLAY[1])));
 
                 return this.turnEntryService.getTurnEntries(TurnType.INSTEAD);
             })
             .then((insteads: Instead[]) => {
                 turns = turns.concat(this.getTurnFormTurnEntries(TurnType.INSTEAD, insteads, 
-                    _.random(PlayService.INSTEADS_BY_PLAY[0], PlayService.INSTEADS_BY_PLAY[1])));
+                    _.random(ENV.INSTEADS_BY_PLAY[0], ENV.INSTEADS_BY_PLAY[1])));
 
                 return this.turnEntryService.getTurnEntries(TurnType.LIST);
             })
             .then((lists: List[]) => {
                 turns = turns.concat(this.getTurnFormTurnEntries(TurnType.LIST, lists, 
-                    _.random(PlayService.LISTS_BY_PLAY[0], PlayService.LISTS_BY_PLAY[1])));
+                    _.random(ENV.LISTS_BY_PLAY[0], ENV.LISTS_BY_PLAY[1])));
 
                 return this.turnEntryService.getTurnEntries(TurnType.LONG_WINDED);
             })
             .then((longWindeds: LongWinded[]) => {
                 longWindedTurns = this.getDecoupledTurnsFormTurnEntries(TurnType.LONG_WINDED, longWindeds, 
-                    _.random(PlayService.LONG_WINDEDS_BY_PLAY[0], PlayService.LONG_WINDEDS_BY_PLAY[1]));
+                    _.random(ENV.LONG_WINDEDS_BY_PLAY[0], ENV.LONG_WINDEDS_BY_PLAY[1]));
 
                 return this.turnEntryService.getTurnEntries(TurnType.SONG);
             })
             .then((songs: Song[]) => {
                 turns = turns.concat(this.getTurnFormTurnEntries(TurnType.SONG, songs, 
-                    _.random(PlayService.SONGS_BY_PLAY[0], PlayService.SONGS_BY_PLAY[1])));
+                    _.random(ENV.SONGS_BY_PLAY[0], ENV.SONGS_BY_PLAY[1])));
  
                 return this.turnEntryService.getTurnEntries(TurnType.CONDITION);
             })
             .then((conditions: Condition[]) => {
                 // Complétion jusqu'à "TURN_NUMBER_TOTAL" avec des conditions
                 turns = turns.concat(this.getTurnFormTurnEntries(TurnType.CONDITION, conditions, 
-                    PlayService.TURN_NUMBER_TOTAL - (turns.length + longWindedTurns.length)));
+                    ENV.TURN_NUMBER_TOTAL - (turns.length + longWindedTurns.length)));
                 
                 return Promise.resolve(_.shuffle(turns));
             })
@@ -81,13 +68,13 @@ export class PlayService {
                 // Tant que tous les tours de longue haleine n'ont pas été insérés
                 while (longWindedTurnsIndex < longWindedTurnsLength) {
                     // Les tours de jeu de longue haleine doivent être tous atteint avant d'avoir effectué un certain pourcentage de tours
-                    const random:number = _.random(Math.round((PlayService.MAX_PERCENT_TURNS_BEFORE_ANY_LONG_WINDED_TURN) * turns.length - (turnsIndex + 1)));
+                    const random:number = _.random(Math.round((ENV.MAX_PERCENT_TURNS_BEFORE_ANY_LONG_WINDED_TURN) * turns.length - (turnsIndex + 1)));
                     if (random <= 0) {
                         // Insertion de la première slide d'un tour de longue haleine
                         turns.splice(turnsIndex, 0, longWindedTurns[longWindedTurnsIndex][0]);
 
                         // Insertion de la deuxième slide d'un tour de longue haleine
-                        const longWindedEndTurnIndex = turnsIndex + _.random(PlayService.MIN_TURNS_AFTER_LONG_WINDED_END, turns.length - (turnsIndex + 1));
+                        const longWindedEndTurnIndex = turnsIndex + _.random(ENV.MIN_TURNS_AFTER_LONG_WINDED_END, turns.length - (turnsIndex + 1));
                         turns.splice(longWindedEndTurnIndex, 0, longWindedTurns[longWindedTurnsIndex][1]);
 
                         // On avance dans le tableau des tours de longue haleine
